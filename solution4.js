83d840521e79514d2ccdb2dbd94efa01f87d2783
mayukhThakuria <155564101+mayukhThakuria@users.noreply.github.com>
2024-06-17 16:00:44 +0530

    final changes

--- solution4.js
+++ solution4.js
@@ -42,36 +42,62 @@ var findMedianSortedArrays1 = function(nums1,nums2){
 
 var findMedianSortedArrays = function(nums1, nums2) {
     if(nums2.length<nums1.length){
-        findMedianSortedArrays(nums2,nums1);
+        return findMedianSortedArrays(nums2,nums1);
+        // we make sure that nums1 has less length
     }
     let n1 = nums1.length;
     let n2 = nums2.length;
+    // storing the lengths
+    // taking care if one of them is empty
+    if(n1==0){
+        if(n2==0){
+            return 0;
+        }
+        else{
+            if(n2%2==1){
+                return nums2[(n2-1)/2]
+            }
+            return (nums2[n2/2-1]+nums2[n2/2])/2
+        }
+    }
     let length = n1 + n2;
     let left = Math.floor((n1+n2+1)/2);
+    // left stores the number of variables that will be on the left of the median
     let i = 0;
     let j = n1;
+    // i and j will iterate over nums1;
     while(i<=j){
         let mid1 = Math.floor((i+j)/2);
+        // we just atke an index on num1
         let mid2 = left - mid1;
+        // find a corresponding index on nums2
         let l1 = Number.MIN_SAFE_INTEGER;
         let l2 = Number.MIN_SAFE_INTEGER;
         let r2 = Number.MAX_SAFE_INTEGER;
         let r1 = Number.MAX_SAFE_INTEGER;
+        // l1 and l2 are the numbers where mid will point..
+        // if l1>r2 and l2>r1 then we have exact number of required numbers from both the arrays to form a median
         if(mid1>0) l1= nums1[mid1-1];
         if(mid2>0) l2= nums2[mid2-1];
         if(mid1<n1) r1 = nums1[mid1];
         if(mid2<n2) r2 = nums2[mid2];
-        if(l2<r1&&l1<r2){
+        if(l2<=r1&&l1<=r2){
             if(length%2==1){
+                // in case of an odd length we can directly return the index
                 return Math.max(l1,l2);
             }
+            // in case of even we return the average
             return (Math.max(l1,l2)+Math.min(r2,r1))/2;
         }
         else if(l2>r1){
             i=mid1+1
+            // if l2 is big we need to take more numbers from nums1 and less from nums1
         }
         else{
             j=mid1-1;
+            // same concept as above
         }
     }
+    // dummy return
+    return 0;
 };
